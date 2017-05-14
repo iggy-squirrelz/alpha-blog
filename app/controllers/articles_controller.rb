d5954f1cb255492567a35b79a75d073864c96cf6
Justin I. Squires <justin.ignacious87@gmail.com>
2017-05-14 17:09:19 -0400

    Error Correction, form now displaying

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,9 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
     @article.save
-    redirect_to articles_show(@article)
+    redirect_to articles_path(@article)
   end
 
   private
